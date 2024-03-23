a3c08f078e05706d6796261cd39f03c210ca58fe
laffra <laffra@gmail.com>
2024-03-23 13:36:03 +0100

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ if [ "$answer" = "y" ] ;then
         git add --all
         git commit -m 'Deploy the latest version'
         git push origin main
-        echo "Latest version was deployed to github. Check DigitalOcean for progress:"
+        echo "Latest version has been deployed to github. Check DigitalOcean for progress:"
         echo
         echo "https://cloud.digitalocean.com/apps/81140869-ab35-47cc-8afd-d72c51dd9cf4/settings?i=0e0d0e"
         echo
