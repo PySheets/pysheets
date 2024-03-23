1a6971fa6840bff31d82f425195e917f94f54893
laffra <laffra@gmail.com>
2024-03-23 13:05:58 +0100

    Push version using commmand line

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,18 @@
-source build.sh
-(cd dist; gcloud app deploy)
+clear
+
+printf  "Did you update the version in app.yaml? (y/n) " yn
+read answer
+
+if [ "$answer" = "y" ] ;then 
+    source build.sh
+    (
+        cd ../pysheets-prod;
+        git add --all
+        git commit -m 'Deploy the latest version'
+        git push origin main
+        echo "Latest version was deployed to github. Check DigitalOcean for progress:"
+        echo
+        echo "https://cloud.digitalocean.com/apps/81140869-ab35-47cc-8afd-d72c51dd9cf4/settings?i=0e0d0e"
+        echo
+    )
+fi
