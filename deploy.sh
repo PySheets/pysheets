18eae1e0b8200b5ba4c04e9c90116798a174d4dc
laffra <laffra@gmail.com>
2024-04-03 22:47:07 +0200

    Cancel deploy when the build/tests fails

--- deploy.sh
+++ deploy.sh
@@ -11,17 +11,24 @@ if [ "$prod" = "$dev" ] ;then
     echo "Update the version. Cmd+Click here:        app.yaml"
     echo "Then run this script again."
 else
-    source build.sh
-    (
-        cd ../pysheets-prod;
-        git add --all
-        git commit -m 'Deploy the latest version'
-        git push origin main
-        echo "Latest version has been deployed to github. Check DigitalOcean for progress:"
-        echo
-        echo "https://cloud.digitalocean.com/apps/81140869-ab35-47cc-8afd-d72c51dd9cf4/settings?i=0e0d0e"
-        echo
-    )
+    ./build.sh
+    if [[ $? -eq 0 ]]; then
+        echo "Build succeeded"
+        (
+            cd ../pysheets-prod;
+            git add --all
+            git commit -m 'Deploy the latest version'
+            git push origin main
+            echo "Latest version has been deployed to github. Check DigitalOcean for progress:"
+            echo
+            echo "https://cloud.digitalocean.com/apps/81140869-ab35-47cc-8afd-d72c51dd9cf4/settings?i=0e0d0e"
+            echo
+        )
+    else
+        echo "Build failed"
+        exit 1
+    fi
+    
 fi
 
 date
