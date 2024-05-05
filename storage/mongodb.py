3562d5ff96c40476a7c10920d5d97624ea4e0997
laffra <laffra@gmail.com>
2024-05-05 21:43:10 +0200

    Remove test code

--- storage/mongodb.py
+++ storage/mongodb.py
@@ -12,17 +12,3 @@ mongo_config = {
     "password": "bmlyXWlRYX15",
 }
 db = MongoClient(**mongo_config)["pysheets"]
-
-
-if __name__ == "__main__":
-    result =db.user.insert_one({
-        "name": "Chris",
-        "email": "laffra@gmail.com",
-    })
-    if not result.inserted_id:
-        print("Failed to insert")
-    print("Users:", list(db.user.find({})))
-    print("DB:", db.user)
-    for user in db.user.find({}):
-        db.user.delete_one({"_id": user["_id"] })
-    print("Users:", list(db.user.find({})))
