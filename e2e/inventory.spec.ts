9f7ebc4c69d9492d838c32704e95fa1fa5fe1bb4
laffra <laffra@gmail.com>
2024-08-27 14:27:05 +0200

    Connect to IP

--- e2e/inventory.spec.ts
+++ e2e/inventory.spec.ts
@@ -5,19 +5,19 @@ test.use({
 });
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:8081/');
+  await page.goto('http://127.0.0.1:8081/');
   // Make sure we have the right title
   await expect(page).toHaveTitle(/PySheets/);
 });
 
 test('has new button', async ({ page }) => {
-  await page.goto('http://localhost:8081/');
+  await page.goto('http://127.0.0.1:8081/');
   // Find the new button and make sure it exists
   await expect(page.locator('.new-button')).toHaveText('New Sheet');
 });
 
 test('create new sheet', async ({ page }) => {
-  await page.goto('http://localhost:8081/');
+  await page.goto('http://127.0.0.1:8081/');
   // Click the new button and create a new sheet
   await page.locator('.new-button').click();
   // Make sure we have the right title
