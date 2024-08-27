c5a83b8fd33f09dee1e945c68f8d589d47188c2b
laffra <laffra@gmail.com>
2024-08-27 23:58:40 +0200

    Upgrade to v0.2.2

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ cp -r LICENSE ../pysheets-prod
 cp -r Procfile ../pysheets-prod
 
 
-# rm dist/*.gz dist/*.whl
-# python3 -m build && twine check dist/* && twine upload dist/*
-# rm -rf src/pysheets_app.egg-info
-# rm dist/*.gz dist/*.whl
+rm dist/*.gz dist/*.whl
+python3 -m build && twine check dist/* && twine upload dist/*
+rm -rf src/pysheets_app.egg-info
+rm dist/*.gz dist/*.whl
