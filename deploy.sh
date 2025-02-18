9ebb5a6daaa039980545b5b7b8c4e173b222e0f6
Chris Laffra <laffra@gmail.com>
2025-02-18 10:18:32 +0100

    Update quack

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,16 @@
 cp -r src/static/lib ../pysheets-prod/static
-cp -r src/static/ltk ../pysheets-prod/static
 cp -r src/static/views ../pysheets-prod/static
 cp -r src/static/icons ../pysheets-prod/static
 cp src/static/*.py ../pysheets-prod/static
 cp src/static/*.png ../pysheets-prod/static
 cp src/static/*.ico ../pysheets-prod/static
 cp src/static/*.css ../pysheets-prod/static
-cp src/static/*.js ../pysheets-prod/static
 cp -r src/templates ../pysheets-prod
 cp -r src/*.py ../pysheets-prod
-cp -r requirements.txt ../pysheets-prod
-cp -r LICENSE ../pysheets-prod
-cp -r Procfile ../pysheets-prod
+cp requirements.txt ../pysheets-prod
+cp LICENSE ../pysheets-prod
+cp Procfile ../pysheets-prod
 mkdir -p ../pysheets-prod/static/css
-cp src/static/lib/css/custom.css ../pysheets-prod/static/css/custom.css
 
 
 rm dist/*.gz dist/*.whl
